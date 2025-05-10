6a3259f2ab0da1e816101ec87d044beb11540c50
Téo J. <105713570+Holiaaa@users.noreply.github.com>
2025-05-10 16:32:03 +0200

    Update Events.cpp

--- Events.cpp
+++ Events.cpp
@@ -1,5 +1,7 @@
 #include "Events.hpp"
 
+// Code repris et légerement modifier de CastleSlash V2 (https://github.com/Holiaaa/CastleSlash-Old/blob/main/source/events.c)
+
 int Events::getInput() {
     int input = getch();
     if (input == Events::KEY_DIR_ALIAS || input == 224) {
